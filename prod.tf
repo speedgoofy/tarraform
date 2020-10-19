8c832845189682424dbbb9b27af538637b86590a
Brandon Mixon <brandonmixon@icloud.com>
2020-10-19 14:00:23 -0500

    added web security

--- prod.tf
+++ prod.tf
@@ -9,3 +9,33 @@ resource "aws_s3_bucket" "prod_tf_bucket_s3" {
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "allow standard http and https ports inbound and everything outbound"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Tarraform" : "true"
+  }
+}
